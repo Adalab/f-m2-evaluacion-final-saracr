2df18d41319e9f99b0d6077cafba4427c9cc761f
“Sara <saracrojas@gmail.com>
2019-04-22 14:37:50 +0200

    Fix url function

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -2,30 +2,27 @@
 
 console.log('>> Ready :)');
 
-
 const input = document.querySelector('.input');
 const button = document.querySelector('.btn');
 const tvShows = document.querySelector('.tv-shows');
 
+const url = 'http://api.tvmaze.com/search/shows?q=';
+const defaultImg = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
+
 //Función que llama al listado de series
 
 function showList(){
-  fetch('http://api.tvmaze.com/search/shows?q=')
-    .then(function(response){
-      return response.json();
-    })
-    .then(function(data){
-      console.log(data.result);
-      for (const result of data.results){
-        const shows = result.shows;
-        tvShows.innerHTML += `<li> ${image} <br> ${name}`;
-      }
-
+  const inputValue = input.Value;
+  fetch(url + inputValue)
+    .then(response => response.json())
+    .then(data => {
+      getList(data);
     });
-
-
+  console.log('wtf');
 }
+// Función que pinte tarjeta de la serie con img y título
 
+function getList(){}
 
 
 // Botón que muestra listado de series
