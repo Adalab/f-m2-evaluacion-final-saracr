4b6baaf2a95715dc6f52dc393351a81df8d52570
“Sara <saracrojas@gmail.com>
2019-04-22 09:36:34 +0200

    Fix html & add js

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -10,52 +10,23 @@ const tvShows = document.querySelector('.tv-shows');
 //Función que llama al listado de series
 
 function showList(){
-    fetch('http://api.tvmaze.com/search/shows?q=:query')
-        .then(function(response){
-            return response.json();
-        })
-        .then(function(data){
-            console.log(data.result)
-            for (const result of data.results){
-                const shows = result.shows;
-                tvShows.innerHTML += `<li> ${image} <br> ${name}`
-            }
+  fetch('http://api.tvmaze.com/search/shows?q=')
+    .then(function(response){
+      return response.json();
+    })
+    .then(function(data){
+      console.log(data.result);
+      for (const result of data.results){
+        const shows = result.shows;
+        tvShows.innerHTML += `<li> ${image} <br> ${name}`;
+      }
 
-        })
+    });
 
-        
+
 }
 
 
 
-
-
-
-
-function search() {
-    fetch(`https://swapi.co/api/people/?search=${input.value}`)
-       .then(function(response) {
-           return response.json();
-       })
-       .then(function(data) {
-           console.log(data.results)
-           for (const result of data.results) {
-           const name = result.name;
-           const gender = result.gender;
-           fetch(result.homeworld)
-           .then(response => response.json())
-           .then(data => {
-                personajillos.innerHTML += `<li>Nombre: ${name} <br> Género: ${gender} <br> Planeta de origen: ${data.name}</li>`
-           });
-       }})
-    
-    }
-    
-    button.addEventListener('click', search);
-
-
-
-
-
-// Botón que muestra listado de series 
-button.addEventListener('click' showList);
+// Botón que muestra listado de series
+button.addEventListener('click',showList);
