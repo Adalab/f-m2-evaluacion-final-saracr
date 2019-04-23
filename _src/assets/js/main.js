c2572dac930b293006bfc9eb1022380a860bfaf0
“Sara <saracrojas@gmail.com>
2019-04-23 14:35:29 +0200

    Fix function li/ add styles

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,13 +1,11 @@
 'use strict';
 
-console.log('>> Ready :)');
-
 const input = document.querySelector('.input');
 const button = document.querySelector('.btn');
-const tvShows = document.querySelector('.tv-shows');
+const tvShows = document.querySelector('.tv__shows');
 
-const url = 'http://api.tvmaze.com/search/shows?q=';
-const defaultImg = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
+const imgDefault = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
+
 
 //Función que llama al listado de series en API
 
@@ -16,26 +14,14 @@ function getList(){
   fetch(`http://api.tvmaze.com/search/shows?q=${input.value}`)
     .then(response => response.json())
     .then(data => {
-      console.log(data);
-
-
-      // Pintar listado de series con títulos e imágenes
-
-      for (const item of data){
-        const show = item.show;
-        const series = document.createElement('li');
-        series.classList.add('results__show');
-        const showTitle = document.createElement('h2');
-        showTitle.classList.add('results__title');
-        const showImg = document.createElement('img');
-        showImg.classList.add('results__img');
-
-        if (show.imge === null){
-          showImg.src = defaultImg;
+      for (const result of data) {
+        if (result.show.image === null) {
+          tvShows.innerHTML += `<h2 class="show__title">${result.show.name}</h2><li class="tv__shows-list"><br><img class="img__show" src="${imgDefault}"<br></li>`;
+        } else {
+          tvShows.innerHTML += `<h2 class="show__title">${result.show.name}</h2><li class="tv__shows-list"><br><img class="img__show" src="${result.show.image.medium}"<br></li>`;
         }
       }
     });
 }
-
 
 button.addEventListener('click',getList);
