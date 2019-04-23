06b44c168e5ff1173685fb07cb6045898c9c462e
“Sara <saracrojas@gmail.com>
2019-04-23 02:57:10 +0200

    Edit js

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -9,21 +9,33 @@ const tvShows = document.querySelector('.tv-shows');
 const url = 'http://api.tvmaze.com/search/shows?q=';
 const defaultImg = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
 
-//Función que llama al listado de series
+//Función que llama al listado de series en API
 
-function showList(){
-  const inputValue = input.Value;
-  fetch(url + inputValue)
+function getList(){
+  tvShows.innerHTML = '';
+  fetch(`http://api.tvmaze.com/search/shows?q=${input.value}`)
     .then(response => response.json())
     .then(data => {
-      getList(data);
+      console.log(data);
+
+
+      // Pintar listado de series con títulos e imágenes
+
+      for (const item of data){
+        const show = item.show;
+        const series = document.createElement('li');
+        series.classList.add('results__show');
+        const showTitle = document.createElement('h2');
+        showTitle.classList.add('results__title');
+        const showImg = document.createElement('img');
+        showImg.classList.add('results__img');
+
+        if (show.imge === null){
+          showImg.src = defaultImg;
+        }
+      }
     });
-  console.log('data');
 }
-// Función que pinte tarjeta de la serie con img y título
-
-function getList(){}
 
 
-// Botón que muestra listado de series
-button.addEventListener('click',showList);
+button.addEventListener('click',getList);
